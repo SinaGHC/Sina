6334c4c49784ac227c83989785c96862e723b9f8
SinaGHC <148954119+SinaGHC@users.noreply.github.com>
2023-11-14 17:23:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const username = document.getElementById('username1');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const btn = document.getElementById('button');
+const header = document.getElementById('h11');
+username.addEventListener('input', updateHeader);
+password.addEventListener('input', updateHeader);
 
-username.addEventListener('focus', function () {
-    username.placeholder = '';
-});
-username.addEventListener('blur', function () {
-    if (username.value === '') {
-        username.placeholder = 'username';
+btn.addEventListener('click', function() {
+    if (username.value === '' || password.value === '') {
+        header.textContent = 'Login here. username';
+    } else if (username.value === 'Erfan' && password.value === 'erfanrobot') {
+        header.textContent = 'Welcome ErfanRobot';
+    } else {
+        header.textContent = 'Incorrect U or P';
     }
 });
-function showcontent(){
-    username.value = 'sina';
+function updateHeader(){
+    if (username.value === '' && password.value === '') 
+        header.textContent = 'Login here';
+
 }
